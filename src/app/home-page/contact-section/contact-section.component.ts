c9019ef20dffb4bc5fc3a4d3b974854a2dc15dfd
kamlesh.kalal <kamlesh.kalal@volansys.com>
2023-05-01 18:35:52 +0530

    contact

--- src/app/home-page/contact-section/contact-section.component.ts
+++ src/app/home-page/contact-section/contact-section.component.ts
@@ -1,4 +1,8 @@
+import { FormBuilder, FormControl } from '@angular/forms';
+import { ContactService } from 'app/services/contact.service';
+
 import { Component, OnInit } from '@angular/core';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-contact-section',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactSectionComponent implements OnInit {
 
-  constructor() { }
+  ContactForm: any;
 
-  ngOnInit(): void {
+
+  constructor(private contactService: ContactService, private fb: FormBuilder, private toast: NgToastService) {
+    this.ContactForm = fb.group({
+      contactname: new FormControl(''),
+      contactemail: new FormControl(''),
+      contactnumber: new FormControl(''),
+      contactmessage: new FormControl(''),
+
+    })
+
+  }
+
+  ngOnInit() {
+  }
+  OnSubmit(data: any) {
+    console.log('object :>> ', this.ContactForm.value);
+    this.contactService.postContact(this.ContactForm.value).subscribe({
+      next:(res: any)=>{
+        console.log(res)
+        this.showInfo()
+      },
+      error: (e: any) => {
+        console.log("error::", e)
+
+
+      }
+    });
+  }
+
+
+  showInfo() {
+    this.toast.info({ detail: "INFO", summary: 'Your Request is Inserted ', sticky: true });
   }
 
 }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -15,4 +15,7 @@ export class ContactService {
   deleteContact(id: any): Observable<any> {
     return this.http.delete(`${this.baseUrl}/deletecontact/${id}`);
   }
+  postContact(data:any){
+    return this.http.post(this.baseUrl+'/postcontact',data)
+  }
 }
