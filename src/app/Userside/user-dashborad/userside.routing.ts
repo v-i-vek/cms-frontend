e880dc5c75f3c55d10aff9b2025067302e9abc74
archeee doshi <doshiarchee81@gmail.com>
2023-05-02 11:15:33 +0530

    loader is added to website

--- src/app/Userside/user-dashborad/userside.routing.ts
+++ src/app/Userside/user-dashborad/userside.routing.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { PostsComponent } from '../userdashboard-home/posts/posts.component';
 import { UserdashboardHomeComponent } from '../userdashboard-home/userdashboard-home.component';
-
 import { ProfileuserComponent } from '../userdashboard-home/profileuser/profileuser.component';
 import { EditProfileComponent } from '../userdashboard-home/profileuser/edit-profile/edit-profile.component';
 import { LogoutComponent } from 'app/logout/logout.component';
 import { LogoutUserComponent } from '../userdashboard-home/logout-user/logout-user.component';
+import { UserservicesComponent } from '../userdashboard-home/userservices/userservices.component';
 
 
 
@@ -14,6 +14,7 @@ export const userLayoutRoutes: Routes = [
     {path:'profile', component:ProfileuserComponent},
     {path:'Edit', component:EditProfileComponent},     
     {path:'propertydetails', component:PostsComponent},
+    {path:'services', component:UserservicesComponent},
     {path:'logoutuser', component:LogoutUserComponent},
       
   
