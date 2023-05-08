f7bf5659403ecc2721d15a0afe695d0376e4bcfa
insert github real name here <insert github email here>
2023-05-08 07:41:57 +0530

    userside services

--- src/app/Userside/userdashboard-home/userservices/userservices.component.ts
+++ src/app/Userside/userdashboard-home/userservices/userservices.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ManageUserServiceService } from 'app/services/manageUserService.service';
 import { Router } from '@angular/router';
 import { SerServiceService } from 'app/services/serService.service';
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-userservices',
@@ -10,26 +10,46 @@ import { SerServiceService } from 'app/services/serService.service';
   styleUrls: ['./userservices.component.scss']
 })
 export class UserservicesComponent implements OnInit {
-  userId:any
-  userData:any
-  serviceDataDisplay: any;
+  userId:any;
+  userData:any;
+  serviceDataDisplay: any[];
+  selectedServices: any[] = [];
+  name: string;
+  description: string;
+  customize: string;
+  serviceimage: string;
 
-  constructor(private serServiceService :SerServiceService) {
-    this.getserviceDetails()
-   }
+  constructor(private serServiceService :SerServiceService, private http: HttpClient) {
+    this.getserviceDetails();
+  }
 
   ngOnInit(): void {
   }
-  getserviceDetails(){
 
+  getserviceDetails(){
     this.serServiceService.serviceGet().subscribe(
       (res: any) => {
-            this.serviceDataDisplay = res
-           console.log(this.serviceDataDisplay)
-            
-        }
-   // (e)=>console.log("error::",e)
-    )
+        this.serviceDataDisplay = res;
+        console.log(this.serviceDataDisplay);
+      }
+    );
   }
 
-}
+  addService() {
+    const newService = {
+      name: 'Service Name',
+      description: 'Service Description'
+    };
+  
+    this.http.post('http://localhost:3000/api/services', newService).subscribe(
+      (response) => {
+        console.log('Service added successfully');
+      },
+      (error) => {
+        console.error('Error adding service:', error);
+      }
+    );
+  }
+}  
+
+
